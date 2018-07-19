5e471a8f2cbdf67c62cc1695b9d63709f12239fa
EC2 Default User <ec2-user@ip-172-31-17-121.ec2.internal>
2018-07-19 13:00:37 +0000

    Fixed another "class" entry
     - tweaked the transcribing animation so that it doesn't make
       the other elements jiggle

--- src/components/AllNotes.jsx
+++ src/components/AllNotes.jsx
@@ -172,8 +172,6 @@ class AllNotes extends Component {
                                 note.text === '-' ? (
                                     <div className='load-2'>
                                         <div className='line'></div>
-                                        <div className='line'></div>
-                                        <div className='line'></div>
                                     </div>
                                 ) : (
                                     note.text
@@ -181,7 +179,7 @@ class AllNotes extends Component {
                             )}
                         </td>
                         <td>
-                            <a class="btn red" onClick={this.handleDelete.bind(this, note)}>Delete</a>
+                            <a className="btn red" onClick={this.handleDelete.bind(this, note)}>Delete</a>
                         </td>
                     </tr>
                 ) : (
