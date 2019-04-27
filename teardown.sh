cb53dd5e4e8439242054f05e20c9134c7aab5fa4
EC2 Default User <ec2-user@ip-172-31-52-195.ec2.internal>
2019-04-27 05:09:31 +0000

    Removed old step from teardown

--- teardown.sh
+++ teardown.sh
@@ -3,6 +3,5 @@
 # reset the lambda code
 aws lambda update-function-code --function-name transcriber --zip-file fileb://docs/empty-lambda.zip
 amplify delete
-aws s3 rb s3://lab-content-notable --force
 rm src/aws-exports.js
 git reset --hard
